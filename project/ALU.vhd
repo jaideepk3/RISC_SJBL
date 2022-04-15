e447c48c9a1d6dd9f12a0e8be4236a66c78d3fdb
Bhuvana Chandra <86913436+Bhuvana-Chandra@users.noreply.github.com>
2022-04-15 11:17:20 +0530

    Update ALU.vhd

--- project/ALU.vhd
+++ project/ALU.vhd
@@ -7,7 +7,7 @@ entity alu is
 	 inp1 : in std_logic_vector(15 downto 0);
 	 inp2 : in std_logic_vector(15 downto 0); 
 	 op_sel : in std_logic_vector(1 downto 0);
-	 output : out std_logic_vector(15 downto 0);
+	 outp : out std_logic_vector(15 downto 0);
 	 c : out std_logic;                             ---overflow flag
 	 z : out std_logic
 );
